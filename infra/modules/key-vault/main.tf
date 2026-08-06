776ebae8398f9cf1da8be180afd51e7c91ad9155
Deepak Kumar <deepak.ab.kumar0408@gmail.com>
2026-08-06 12:14:54 +0200

    Corrected syntax issues

--- infra/modules/key-vault/main.tf
+++ infra/modules/key-vault/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_key_vault" "this" {
 }
 
 resource "azurerm_role_assignment" "kv_admin" {
-  scope = azurerm_key_vault.kv.this.id
+  scope = azurerm_key_vault.this.id
   role_definition_name = "Key Vault Administrator"
   principal_id = data.azurerm_client_config.current.object_id
 }
