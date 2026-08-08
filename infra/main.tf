245d04c2d98a2405305d5549e6735c57f7f505fe
Deepak Kumar <deepak.ab.kumar0408@gmail.com>
2026-08-08 19:43:40 +0000

    Creating ACI for both backend and frontend container#3

--- infra/main.tf
+++ infra/main.tf
@@ -58,6 +58,8 @@ module "bytebrain_backend_container" {
 
   name                = "bytebrain-backend"
   container_name      = "backend"
+  identity_name       = module.bytebrain_user_assigned_identity.name
+  identity_resource_group_name = module.bytebrain_user_assigned_identity.name
   resource_group_name = module.bytebrain_rg.name
   location            = module.bytebrain_rg.location
   container_image     = "${module.bytebrain_acr.login_server}/${var.backend_image_name}:${var.backend_image_tag}"
@@ -86,6 +88,8 @@ module "bytebrain_frontend_container" {
   container_name      = "frontend"
   resource_group_name = module.bytebrain_rg.name
   location            = module.bytebrain_rg.location
+  identity_name       = module.bytebrain_user_assigned_identity.name
+  identity_resource_group_name = module.bytebrain_user_assigned_identity.name
   container_image     = "${module.bytebrain_acr.login_server}/${var.frontend_image_name}:${var.frontend_image_tag}"
   cpu                 = var.frontend_cpu
   memory              = var.frontend_memory
