e6405bd04291adf9783cc1d55d50775f61b869df
Deepak Kumar <deepak.ab.kumar0408@gmail.com>
2026-08-11 21:06:52 +0000

    Fixed VM publisher issues

--- infra/modules/jump-vm-aks/main.tf
+++ infra/modules/jump-vm-aks/main.tf
@@ -62,7 +62,7 @@ resource "azurerm_linux_virtual_machine" "jumpbox" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
+    offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts-gen2"
     version   = "latest"
   }
