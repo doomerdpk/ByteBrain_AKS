c1cbc1e72c5d772b01638df846fbe17b4ec15e29
Deepak Kumar <deepak.ab.kumar0408@gmail.com>
2026-08-13 22:34:27 +0000

    Modified node permissions

--- infra/modules/aks/main.tf
+++ infra/modules/aks/main.tf
@@ -159,3 +159,8 @@ resource "azurerm_role_assignment" "kubelet_kv_secrets_user" {
   principal_id         = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
 }
 
+resource "azurerm_role_assignment" "kubelet_acr_pull" {
+  scope                = var.acr_id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+}
